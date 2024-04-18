8d081ad12a862636de67c211aca2582fce20ce1e
Lennart Mol <lc.mol@student.avans.nl>
2024-04-18 17:06:36 +0200

    Automatically subscribes topic_list

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import prototype
 
-mqtt_thingy = prototype.MQTT('mqtt-dashboard.com', 'test/hits')
+mqtt_thingy = prototype.MQTT('mqtt-dashboard.com', ['test/hits', 'test/config'])
 
 mqtt_thingy.start()
-mqtt_thingy.publish("test/hits", "Hello World!")
+mqtt_thingy.publish("test/hits", "Somebody has been hit!")
 
 while True:
+    # send something from server 'test/config' topic to receive it here
     pass
--- prototype.py
+++ prototype.py
@@ -4,15 +4,16 @@ class MQTT():
 
     def __init__(self, broker_ip, topic_list):
         self.BROKER_ADDR = broker_ip
-        self.TEST_TOPIC = topic_list
+        self.TOPICS = topic_list
         self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     
     # The callback for when the client receives a CONNACK response from the server.
     def on_connect(self, client, userdata, flags, reason_code, properties):
-        #print(f"Connected with result code {reason_code}")
+        print(f"Connected with result code {reason_code}")
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        client.subscribe(self.TEST_TOPIC)
+        for t in self.TOPICS:
+            client.subscribe(t)
 
     # The callback for when a PUBLISH message is received from the server.
     def on_message(self, client, userdata, msg):
