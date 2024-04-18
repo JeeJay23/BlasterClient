6ce6bc5e6c20b3c9c7c4be85c4cf5d970ed43d00
Lennart Mol <lc.mol@student.avans.nl>
2024-04-18 17:08:01 +0200

    Change name of file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import prototype
+import mqtt
 
-mqtt_thingy = prototype.MQTT('mqtt-dashboard.com', ['test/hits', 'test/config'])
+mqtt_thingy = mqtt.MQTT('mqtt-dashboard.com', ['test/hits', 'test/config'])
 
 mqtt_thingy.start()
 mqtt_thingy.publish("test/hits", "Somebody has been hit!")
