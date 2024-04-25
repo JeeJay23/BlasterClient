e3740ad2769447bff26f3216635b470b977f3533
Lennart Mol <lc.mol@student.avans.nl>
2024-04-25 14:38:18 +0200

    Some callbacks or smth

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import mqtt
+from uuid import getnode as get_mac
 
-def message_handler(topic, message):
-    print(f"Received message from {topic}: {message}")
-    # TODO handle messages from server
+class Blaster():
 
-id = 1
+    def __init__(self):
+        self.config = None # TODO set to config object
+        self.game_state = False # Set to true after receiving 'Start' from hub
+        self.id = get_mac()
 
-server_ip = 'mqtt-dashboard.com' # to be set to the server IP
+        # this will later all be retreived from config -> pass object to mqtt class
+        self.blaster_topic = 'blaster/'+str(self.id)
+        self.list_of_publish_topics = ['blaster/config', 'hub/hits']
+        # create dictory of subscribe topics
+        self.list_of_subscribe_topics = { "config_topic": "blaster/config", "gameplay_topic": "hub/gameplay", "blaster_topic": self.blaster_topic }
+        self.server_ip = 'mqtt-dashboard.com' 
+        self.mqtt_thingy = mqtt.MQTT(self.server_ip, self.list_of_subscribe_topics, self.register_ack_callback)
+        self.mqtt_thingy.start()
 
-blaster_topic = 'blaster/'+str(id)
+        self.ack_received = False
 
-list_of_publish_topics = ['blaster/config', 'hub/hits']
 
-list_of_subscribe_topics = ['blaster/config', 'hub/gameplay', blaster_topic]
+    def initialize_blaster(self):
+        self.mqtt_thingy.register_id(self.id)
+        while not self.ack_received:
+            pass # blocking
+        self.ack_received = False
+        pass
 
-mqtt_thingy = mqtt.MQTT(server_ip, list_of_subscribe_topics, message_handler)
+    def register_ack_callback(self):
+        self.ack_received = True
 
-mqtt_thingy.start()
 
-mqtt_thingy.publish("blaster/config", f'{{"cmd": "register", "id": {id}}}')
-
+blaster_gun_thingy = Blaster()
+blaster_gun_thingy.initialize_blaster()
 
 while True:
-    # send something from server 'test/config' topic to receive it here
-    
-    
-    pass
+    pass # blocking
--- mqtt.py
+++ mqtt.py
@@ -1,24 +1,48 @@
 import paho.mqtt.client as mqtt
+import json
 
 class MQTT():
 
-    def __init__(self, broker_ip, topic_list, message_callback):
+    def __init__(self, broker_ip, topic_list, register_ack_callback):
         self.BROKER_ADDR = broker_ip
         self.TOPICS = topic_list
         self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-        self.message_callback = message_callback
+        self.register_ack_callback = register_ack_callback
     
     # The callback for when the client receives a CONNACK response from the server.
     def on_connect(self, client, userdata, flags, reason_code, properties):
         print(f"Connected with result code {reason_code}")
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        for t in self.TOPICS:
-            client.subscribe(t)
+        
+        # Subscribe to all topics in the list
+        for topic in self.TOPICS.values():
+            client.subscribe(topic)
 
     # The callback for when a PUBLISH message is received from the server.
     def on_message(self, client, userdata, msg):
-        self.message_callback(msg.topic, msg.payload.decode())
+        message = msg.payload.decode()
+        topic = msg.topic
+
+
+        print(f"Received message from {topic}: {message}")
+        # json_object to receive
+        #   {
+        #       "cmd": "ack",
+        #       "id": 0 // id of the hub
+        #       "cmdAck": "regID" // command that was acknowledged
+        #   }
+        # -> { "cmd": "ack", "id": 0, "cmdAck": "regID" }
+
+        # convert message_string to json object
+        json_object = json.loads(message)
+
+        # Handle ack messages
+        if topic == self.TOPICS['blaster_topic']:
+
+            # Handle ack message for registration
+            if json_object['cmd'] == 'ack' and json_object['cmdAck'] == 'regID':
+                self.register_ack_callback()
 
 
     def start(self):
@@ -27,5 +51,6 @@ class MQTT():
         self.mqttc.connect(self.BROKER_ADDR, 1883, 60)
         self.mqttc.loop_start()
 
-    def publish(self, topic, message):
-        self.mqttc.publish(topic, message)
+
+    def register_id(self, id):
+        self.mqttc.publish("blaster/config", f'{{"cmd": "regID", "id": {id}}}')
