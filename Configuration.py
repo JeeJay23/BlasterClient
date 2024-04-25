ddec26f46ea3120fb35b0630f8bad19ffb800ca7
Jader <jvjader@gmail.com>
2024-04-25 14:58:57 +0200

    finished config obj

--- Configuration.py
+++ Configuration.py
@@ -2,5 +2,16 @@ import json
 
 class Configuration(object):
     def __init__(self, filepath) -> None:
-        with open(filepath) as file:
-            print(file)
+        with open(filepath, mode='r') as file:
+            self.configJson = json.loads(file.read())
+            print(self.configJson.get('ID'))
+    
+    def get(self, key):
+        return self.configJson[key]
+
+    def set(self, key, value):
+        self.configJson[key] = value;
+
+    def write(self, filepath):
+        with open(filepath, mode='w') as file:
+            file.write(json.dumps(self.configJson, indent='\t'))
