f8ad7b20941159e82ea296ec7a7835b09092ef2d
Lennart Mol <lc.mol@student.avans.nl>
2024-04-25 14:57:14 +0200

    Callbacks galore

--- main.py
+++ main.py
@@ -14,21 +14,20 @@ class Blaster():
         # create dictory of subscribe topics
         self.list_of_subscribe_topics = { "config_topic": "blaster/config", "gameplay_topic": "hub/gameplay", "blaster_topic": self.blaster_topic }
         self.server_ip = 'mqtt-dashboard.com' 
-        self.mqtt_thingy = mqtt.MQTT(self.server_ip, self.list_of_subscribe_topics, self.register_ack_callback)
+        self.mqtt_thingy = mqtt.MQTT(self.id, self.server_ip, self.list_of_subscribe_topics, self.register_ack_callback, self.request_config_callback)
         self.mqtt_thingy.start()
 
         self.ack_received = False
 
 
     def initialize_blaster(self):
-        self.mqtt_thingy.register_id(self.id)
-        while not self.ack_received:
-            pass # blocking
-        self.ack_received = False
-        pass
+        self.mqtt_thingy.register_id()
 
     def register_ack_callback(self):
-        self.ack_received = True
+        self.mqtt_thingy.request_config()
+    
+    def request_config_callback(self, config):
+        pass
 
 
 blaster_gun_thingy = Blaster()
--- mqtt.py
+++ mqtt.py
@@ -3,18 +3,20 @@ import json
 
 class MQTT():
 
-    def __init__(self, broker_ip, topic_list, register_ack_callback):
+    def __init__(self, id, broker_ip, topic_list, register_ack_callback, request_config_callback):
+        self.id = id
         self.BROKER_ADDR = broker_ip
         self.TOPICS = topic_list
         self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self.register_ack_callback = register_ack_callback
+        self.request_config_callback = request_config_callback
     
     # The callback for when the client receives a CONNACK response from the server.
     def on_connect(self, client, userdata, flags, reason_code, properties):
         print(f"Connected with result code {reason_code}")
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        
+
         # Subscribe to all topics in the list
         for topic in self.TOPICS.values():
             client.subscribe(topic)
@@ -26,13 +28,30 @@ class MQTT():
 
 
         print(f"Received message from {topic}: {message}")
-        # json_object to receive
+        
+        # example JSON messages
+        # json_object cmdAck to receive
         #   {
         #       "cmd": "ack",
         #       "id": 0 // id of the hub
         #       "cmdAck": "regID" // command that was acknowledged
         #   }
         # -> { "cmd": "ack", "id": 0, "cmdAck": "regID" }
+        
+        # example JSON messages
+        # json_object settings to receive
+        #   {
+        #       "cmd": "setSettings",
+        #       "id": 0 // id of the hub
+        #       "someSettings": 
+        #           {
+        #               "setting1": "value1",   
+        #               "setting2": "value2"
+        #           }
+        #   }
+        # -> { "cmd": "setSettings", "id": 0, "someSettings": { "setting1": "value1", "setting2": "value2" } }
+
+
 
         # convert message_string to json object
         json_object = json.loads(message)
@@ -44,6 +63,12 @@ class MQTT():
             if json_object['cmd'] == 'ack' and json_object['cmdAck'] == 'regID':
                 self.register_ack_callback()
 
+            if json_object['cmd'] == 'setSettings':
+                self.request_config_callback(json_object)
+                
+            
+
+
 
     def start(self):
         self.mqttc.on_connect = self.on_connect
@@ -52,5 +77,8 @@ class MQTT():
         self.mqttc.loop_start()
 
 
-    def register_id(self, id):
-        self.mqttc.publish("blaster/config", f'{{"cmd": "regID", "id": {id}}}')
+    def register_id(self):
+        self.mqttc.publish("blaster/config", f'{{"cmd": "regID", "id": {self.id}}}')
+    
+    def request_config(self):
+        self.mqttc.publish("blaster/config", f'{{"cmd": "reqSettings", "id": {self.id}}}')
