fef3df143993111f4c78716027024c9b47ec9265
Lenny <lc.mol@student.avans.nl>
2024-05-16 13:59:45 +0200

    LED thingy

--- BlasterClient.py
+++ BlasterClient.py
@@ -1,18 +1,30 @@
-import sys
-from uuid import getnode as get_mac
-from Configuration import Configuration
-from Blaster import Blaster
-from mqtt import MQTT
+# import sys
+# from uuid import getnode as get_mac
+# from Configuration import Configuration
+# from Blaster import Blaster
+# from mqtt import MQTT
+from trigger import Trigger
+
+def trigger_callback_pressed():
+    print('This button pressed calledback is called in main')
+
+def trigger_callback_released():
+    print('This button released calledback is called in main')
 
 if __name__ == "__main__":
-    settings_filepath = sys.argv[1]
+    # settings_filepath = sys.argv[1]
 
-    config = Configuration.createTestConfig();
-    config.set("ID", get_mac())
+    # config = Configuration.createTestConfig()
+    # config.set("ID", get_mac())
 
-    client = MQTT(config)
+    # client = MQTT(config)
 
-    blaster = Blaster(config, client)
+    # blaster = Blaster(config, client)
+
+    trig = Trigger(button_pin=26, 
+                    led_pin=13,
+                    trigger_callback_pressed=trigger_callback_pressed, 
+                    trigger_callback_released=trigger_callback_released)
 
     while True:
         pass
--- trigger.py
+++ trigger.py
@@ -1,12 +1,14 @@
 from gpiozero import Button
+from gpiozero import LED
 from time import sleep
 
 class Trigger():
     
-    def __init__(self, button_pin, trigger_callback_pressed, trigger_callback_released, debounce_time=0.05):
-        self.button_pin = button_pin
+    def __init__(self, button_pin, led_pin, trigger_callback_pressed, trigger_callback_released, debounce_time=0.05):
         self.debounce_time = debounce_time
-        self.button = Button(self.button_pin)
+        self.button = Button(button_pin)
+        self.led = LED(led_pin)
+        self.led.off()
         self.button.when_pressed = self.on_pressed
         self.button.when_released = self.on_released
         self.trigger_callback_pressed = trigger_callback_pressed
@@ -16,8 +18,10 @@ class Trigger():
         sleep(self.debounce_time)  # Debounce the signal
         if self.button.is_pressed:
             self.trigger_callback_pressed()
+            self.led.on()
             print('Button pressed wooo')
 
     def on_released(self):
         self.trigger_callback_released()
+        self.led.off()
         print('Button released wooo')
