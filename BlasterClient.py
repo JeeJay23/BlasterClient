58a266a5d4f08c9c20d2df522a3f503f4fa6589c
Lenny <lc.mol@student.avans.nl>
2024-05-16 14:31:06 +0200

    Done with trigger and LED

--- BlasterClient.py
+++ BlasterClient.py
@@ -1,28 +1,35 @@
-# import sys
-# from uuid import getnode as get_mac
-# from Configuration import Configuration
-# from Blaster import Blaster
+import sys
+from uuid import getnode as get_mac
+from Configuration import Configuration
+from Blaster import Blaster
 # from mqtt import MQTT
 from trigger import Trigger
+from gpiozero import LED
 
 def trigger_callback_pressed():
+    led.on()
     print('This button pressed calledback is called in main')
 
 def trigger_callback_released():
+    led.off()
     print('This button released calledback is called in main')
 
 if __name__ == "__main__":
-    # settings_filepath = sys.argv[1]
+    settings_filepath = sys.argv[1]
 
-    # config = Configuration.createTestConfig()
-    # config.set("ID", get_mac())
+    config = Configuration.createTestConfig()
+    config.set("ID", get_mac())
 
-    # client = MQTT(config)
+    client = MQTT(config)
 
-    # blaster = Blaster(config, client)
+    blaster = Blaster(config, client)
+    
+    # initialize led and turn off by default
+    led = LED(13)
+    led.off()
 
-    trig = Trigger(button_pin=26, 
-                    led_pin=13,
+    # initialize trigger
+    trig = Trigger(button_pin=6, 
                     trigger_callback_pressed=trigger_callback_pressed, 
                     trigger_callback_released=trigger_callback_released)
 
