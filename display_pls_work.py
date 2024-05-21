e2828fba401085adbca83e0cce5967645bf0994f
Jader <jvjader@gmail.com>
2024-05-21 14:54:31 +0200

    Missed notification on LCD screen

--- display_pls_work.py
+++ display_pls_work.py
@@ -7,13 +7,13 @@ class Display():
         self.name = name
         self.score = score
         self.hit = False
+        self.missed = False
         self.update_display()
 
     ## TODO update_score needs a parameter 'score' to update the score to the score the server replies back
     def update_score(self):
         self.score = self.score + 1
         #self.score = score
-        self.update_display()
 
     def update_name(self, name):
         self.name = name
@@ -21,7 +21,15 @@ class Display():
 
     def update_display(self):
         self.lcd.text(self.name, 1)
-        line_2_text = "Score: {}{}".format(self.score, " - Hit!" if self.hit else "")
+
+        if self.hit:
+            status = " - Hit!"
+        elif self.missed:
+            status = " - Miss!"
+        else:
+            status = ""
+
+        line_2_text = "Score: {}{}".format(self.score, status)
         self.lcd.text(line_2_text, 2)
 
     def clear_display(self):
